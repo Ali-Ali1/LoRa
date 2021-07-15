8ba2b882bd93ec738459f0d86527ee1ac3e07b7d
Ali Ali <41684444+Ali-Ali1@users.noreply.github.com>
2021-07-15 18:45:58 -0400

    Add files via upload

---
+++ LoRa_2/Simulations/Sims/SimulationProcess.py
@@ -0,0 +1,90 @@
+import simpy
+
+from Framework import PropagationModel
+from Framework.AirInterface import AirInterface
+from Framework.EnergyProfile import EnergyProfile
+from Framework.Gateway import Gateway
+from Framework.LoRaParameters import LoRaParameters
+from Framework.Node import Node
+from Framework.Node_2 import Node as Node_2
+from Framework.SNRModel import SNRModel
+from Simulations.GlobalConfig import *
+
+tx_power_mW = {2: 91.8, 5: 95.9, 8: 101.6, 11: 120.8, 14: 146.5}
+rx_measurements = {'pre_mW': 8.2, 'pre_ms': 3.4, 'rx_lna_on_mW': 39,
+                   'rx_lna_off_mW': 34,
+                   'post_mW': 8.3, 'post_ms': 10.7}
+
+
+def run_helper(args):
+    return run(*args)
+
+
+def run(locs, p_size, sigma, sim_time, gateway_location, num_nodes, transsmission_rate, confirmed_messages, adr):
+
+    sim_env = simpy.Environment()
+    gateway = Gateway(sim_env, gateway_location, max_snr_adr=True, avg_snr_adr=False)
+    nodes = []
+    energy_profile_n = EnergyProfile(5.7e-3, 15, tx_power_mW,
+                                   rx_power=rx_measurements)
+    lora_param_n = LoRaParameters(freq=np.random.choice(LoRaParameters.DEFAULT_CHANNELS),
+                                sf=_sf,
+                                bw=125, cr=5, crc_enabled=1, de_enabled=0, header_implicit_mode=0, tp=14)
+    air_interface_2 = AirInterface(gateway, PropagationModel.LogShadow(std=sigma), SNRModel(), sim_env)
+    node_2 = Node_2(1, energy_profile_n, lora_param_n, sleep_time=(8 * p_size / transmission_rate),
+                    process_time=5,
+                    adr=adr,
+                    location=locs[1],
+                    base_station=gateway, env=sim_env, payload_size=p_size, air_interface=air_interface_2,
+                    confirmed_messages=confirmed_messages)
+    air_interface_1 = AirInterface(node_2, PropagationModel.LogShadow(std=sigma), SNRModel(), sim_env)
+    
+
+
+    for node_id in range(num_nodes):
+        energy_profile = EnergyProfile(5.7e-3, 15, tx_power_mW,
+                                       rx_power=rx_measurements)
+        _sf = np.random.choice(LoRaParameters.SPREADING_FACTORS)
+        if start_with_fixed_sf:
+            _sf = start_sf
+        lora_param = LoRaParameters(freq=np.random.choice(LoRaParameters.DEFAULT_CHANNELS),
+                                    sf=_sf,
+                                    bw=125, cr=5, crc_enabled=1, de_enabled=0, header_implicit_mode=0, tp=14)
+        node = Node(node_id, energy_profile, lora_param, sleep_time=(8 * p_size / transmission_rate),
+                    process_time=5,
+                    adr=adr,
+                    location=locs[node_id],
+                    base_station=gateway, env=sim_env, payload_size=p_size, air_interface=air_interface_1,
+                    confirmed_messages=confirmed_messages)
+        nodes.append(node)
+        sim_env.process(node.run())
+
+    
+
+    sim_env.run(until=sim_time)
+
+    # Simulation is done.
+    # process data
+
+    mean_energy_per_bit_list = list()
+    for n in nodes:
+        mean_energy_per_bit_list.append(n.energy_per_bit())
+
+    data_mean_nodes = Node.get_mean_simulation_data_frame(nodes, name=sigma) / (
+        num_nodes)
+
+    data_gateway = gateway.get_simulation_data(name=sigma) / num_nodes
+    data_air_interface = air_interface.get_simulation_data(name=sigma) / (
+        num_nodes)
+
+
+
+    return {
+        'mean_nodes': data_mean_nodes,
+        'gateway': data_gateway,
+        'air_interface': data_air_interface, # Delete this one
+        'path_loss_std': sigma,
+        'payload_size': p_size,
+        'mean_energy_all_nodes': mean_energy_per_bit_list
+
+    }
